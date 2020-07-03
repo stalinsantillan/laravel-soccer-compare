2a4be3b62c7ccc4c8ee4c911348a423ef01ce374
kcaribou <55296314+kcaribou@users.noreply.github.com>
2020-07-02 19:00:06 -0700

    split admin and user panel and update change_password

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
-Route::redirect('/', 'admin/home');
+
+Route::get('/', function(){
+    if (\Auth::user() && \Auth::user()->hasRole("administrator"))
+        return redirect()->to("/admin/dashboard");
+    return redirect()->to("/user/dashboard");
+});
 
 Auth::routes(['register' => false]);
 
@@ -8,10 +13,15 @@ Route::get('change_password', 'Auth\ChangePasswordController@showChangePasswordF
 Route::patch('change_password', 'Auth\ChangePasswordController@changePassword')->name('auth.change_password');
 
 Route::group(['middleware' => ['auth'], 'prefix' => 'admin', 'as' => 'admin.'], function () {
+    Route::get('dashboard', 'Admin\DashboardController@index')->name('dashboard');
     Route::resource('permissions', 'Admin\PermissionsController');
     Route::delete('permissions_mass_destroy', 'Admin\PermissionsController@massDestroy')->name('permissions.mass_destroy');
     Route::resource('roles', 'Admin\RolesController');
     Route::delete('roles_mass_destroy', 'Admin\RolesController@massDestroy')->name('roles.mass_destroy');
     Route::resource('users', 'Admin\UsersController');
     Route::delete('users_mass_destroy', 'Admin\UsersController@massDestroy')->name('users.mass_destroy');
+});
+
+Route::group(['middleware' => ['auth'], 'prefix' => 'user', 'as' => 'user.'], function () {
+    Route::get('dashboard', 'User\DashboardController@index')->name('dashboard');
 });
