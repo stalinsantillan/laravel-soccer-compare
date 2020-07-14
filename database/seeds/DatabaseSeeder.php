44f05a165b6b2a5f1ce7603c5c29253fd21942e9
Devsolution <q3construction1@gmail.com>
2020-07-14 16:48:19 +0800

    Paramsetting seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(PermissionSeed::class);
         $this->call(RoleSeed::class);
         $this->call(UserSeed::class);
+        $this->call(ParamsettingSeed::class);
     }
 }
