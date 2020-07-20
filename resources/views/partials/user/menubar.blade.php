5e06a0e7f75694310c972259e38bf37eedbec67b
kcaribou <55296314+kcaribou@users.noreply.github.com>
2020-07-19 23:53:41 -0700

    Menu bar update

--- resources/views/partials/user/menubar.blade.php
+++ resources/views/partials/user/menubar.blade.php
@@ -2,31 +2,6 @@
     <div id="navigation">
         <!-- Navigation Menu-->
         <ul class="navigation-menu">
-            {{-- <li class="has-submenu">
-                <a href="#">
-                    <i class="fa-fw fas fa-users"></i>{{ trans('cruds.userManagement.title') }} <div class="arrow-down"></div>
-                </a>
-                <ul class="submenu">
-                    <li>
-                        <a href="{{ route('admin.permissions.index') }}">
-                            <i class="fa-fw fas fa-unlock-alt"></i>
-                            {{ trans('cruds.permission.title') }}
-                        </a>
-                    </li>
-                    <li>
-                        <a href="{{ route('admin.roles.index') }}">
-                            <i class="fa-fw fas fa-briefcase"></i>
-                            {{ trans('cruds.role.title') }}
-                        </a>
-                    </li>
-                    <li>
-                        <a href="{{ route('admin.users.index') }}">
-                            <i class="fa-fw fas fa-user"></i>
-                            {{ trans('cruds.user.title') }}
-                        </a>
-                    </li>
-                </ul>
-            </li> --}}
             <li>
                 <a href="{{ route('user.add_player') }}">
                     {{ trans('global.add') }} {{ trans('cruds.player.title') }}
