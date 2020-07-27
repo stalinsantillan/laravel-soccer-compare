e09151015121a246ce810d46274de7d87f56b812
kcaribou <55296314+kcaribou@users.noreply.github.com>
2020-07-26 23:58:04 -0700

    User table structure update for approval user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email');
             $table->string('password');
+            $table->string('status');
             $table->string('remember_token')->nullable();
 
             $table->timestamps();
