9b5dc127a9f93599e10e1a5b5ea54b2388c3925d
kcaribou <55296314+kcaribou@users.noreply.github.com>
2020-07-26 23:59:24 -0700

    User Approval middleware

---
+++ app/Http/Middleware/CheckApproved.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CheckApproved
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (auth()->user()->status != "Approve") {
+            return redirect()->route('approval');
+        }
+        return $next($request);
+    }
+}
