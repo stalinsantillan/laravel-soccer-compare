41f4c25b78d421afce9afcd0a9072bffa558157f
kcaribou <55296314+kcaribou@users.noreply.github.com>
2020-07-27 00:01:06 -0700

    user register page

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" >
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="{{ asset('login_assets/style.css') }}">
+    </head>
+    <body>
+        <div class="container-fluid bg-layer" id="userLogSection">
+            <div class="row">
+                <div class="col-12">
+                    <form class="jumbotron mt-5 text-center" role="form" method="POST" action="{{ url('register') }}">
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                        <h2 class="text-center mb-4">Soccer Compare</h2>
+                        @if (count($errors) > 0)
+                            <div class="alert alert-danger" id="alertMessage" role="alert">
+                                @foreach ($errors->all() as $error)
+                                    {{ $error }}
+                                @endforeach
+                            </div>
+                        @endif
+                        <img src="{{ asset('login_assets/bg/user_start.jpg') }}" class="heading-avatar-icon" />
+                        <div class="form-group">
+                            <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}" placeholder="Name">
+                        </div>
+                        <div class="form-group">
+                            <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}" placeholder="Email">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" id="password" name="password" value="{{ old('password') }}" placeholder="Password">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" value="{{ old('password_confirmation') }}" placeholder="Confirm Password">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Register</button>
+                        <a href="{{ url("login") }}" class="text-white-50" style="font-size: 13px;">Go to Sign in</a>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+                integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+                crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+                integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+                crossorigin="anonymous"></script>
+    </body>
+</html>
