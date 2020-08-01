317acf124f6da4ea3ada3aaa80486f5de7bd5878
kcaribou <55296314+kcaribou@users.noreply.github.com>
2020-07-31 18:23:52 -0700

    Subscribe middleware set up

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,5 +57,6 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'approved' => \App\Http\Middleware\CheckApproved::class,
+        'subscribed' => \App\Http\Middleware\Subscribed::class,
     ];
 }
