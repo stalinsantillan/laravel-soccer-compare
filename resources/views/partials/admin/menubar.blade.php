1139a1ebdb765d95f12de796c7060409714159a0
kcaribou <55296314+kcaribou@users.noreply.github.com>
2020-08-03 07:09:35 -0700

    Menu bar update in admin panel

--- resources/views/partials/admin/menubar.blade.php
+++ resources/views/partials/admin/menubar.blade.php
@@ -9,18 +9,18 @@
                 <span class="menu-arrow"></span>
             </a>
             <ul class="nav-second-level" aria-expanded="false">
-                <li>
-                    <a href="{{ route('admin.permissions.index') }}">
-                        <i class="fa-fw fas fa-unlock-alt"></i>
-                        <span> {{ trans('cruds.permission.title') }} </span>
-                    </a>
-                </li>
-                <li>
-                    <a href="{{ route('admin.roles.index') }}">
-                        <i class="fa-fw fas fa-briefcase"></i>
-                        <span> {{ trans('cruds.role.title') }} </span>
-                    </a>
-                </li>
+{{--                <li>--}}
+{{--                    <a href="{{ route('admin.permissions.index') }}">--}}
+{{--                        <i class="fa-fw fas fa-unlock-alt"></i>--}}
+{{--                        <span> {{ trans('cruds.permission.title') }} </span>--}}
+{{--                    </a>--}}
+{{--                </li>--}}
+{{--                <li>--}}
+{{--                    <a href="{{ route('admin.roles.index') }}">--}}
+{{--                        <i class="fa-fw fas fa-briefcase"></i>--}}
+{{--                        <span> {{ trans('cruds.role.title') }} </span>--}}
+{{--                    </a>--}}
+{{--                </li>--}}
                 <li>
                     <a href="{{ route('admin.users.index') }}">
                         <i class="fa-fw fas fa-user"></i>
