caf534076967babf998987a3ffddf965cc90aadc
kcaribou <55296314+kcaribou@users.noreply.github.com>
2020-08-10 01:40:05 -0700

    add Injury table migration

---
+++ database/migrations/2020_08_10_082425_create_injuries_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInjuriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('injuries', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('player_id');
+            $table->string('injury');
+            $table->date('injury_date');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('injuries');
+    }
+}
