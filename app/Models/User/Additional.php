8721869d54791dcdd4f7eb6b5a829b8024900571
kcaribou <55296314+kcaribou@users.noreply.github.com>
2020-08-09 23:00:24 -0700

    add function that get national team and first appearance team name in model

--- app/Models/User/Additional.php
+++ app/Models/User/Additional.php
@@ -14,4 +14,28 @@ class Additional extends Model
     {
         return $this->belongsTo('App\Models\User\Player');
     }
+    public function getNationalTeamName(){
+        $national_id_string = $this->national_team;
+        $national = explode("_", $national_id_string);
+        $national_id = $national[0];
+        $national_name = '';
+        if (sizeof($national) > 1) {
+            $national_name = Team::find($national_id)->name;
+        } else {
+            $national_name = ApiTeam::find($national_id)->team_name;
+        }
+        return $national_name;
+    }
+    public function getFirstAppearanceTeamName(){
+        $first_appearance_id_string = $this->first_appearance_team;
+        $first_appearance = explode("_", $first_appearance_id_string);
+        $first_appearance_id = $first_appearance[0];
+        $first_appearance_team_name = '';
+        if (sizeof($first_appearance) > 1) {
+            $first_appearance_team_name = Team::find($first_appearance_id)->name;
+        } else {
+            $first_appearance_team_name = ApiTeam::find($first_appearance_id)->team_name;
+        }
+        return $first_appearance_team_name;
+    }
 }
