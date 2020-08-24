2d9ba51ee2414c61d654388d396059a938c721c0
kcaribou <kcaribou0428@gmail.com>
2020-08-23 19:59:09 -0700

    add guest role in migration

--- database/seeds/RoleSeed.php
+++ database/seeds/RoleSeed.php
@@ -14,5 +14,8 @@ class RoleSeed extends Seeder
     {
         $role = Role::create(['name' => 'administrator']);
         $role->givePermissionTo('users_manage');
+
+        $role = Role::create(['name' => 'guest']);
+        $role->givePermissionTo('guest_manage');
     }
 }
